73d8924fa51a31779b7bfe9e5226284985548cc2
vivekmantur <vivek.mantur@cognine.com>
2024-07-26 22:39:55 +0530

    Update Program.cs

--- CsharpAssignment/CsharpAssignment3/CsharpAssignment3/Program.cs
+++ CsharpAssignment/CsharpAssignment3/CsharpAssignment3/Program.cs
@@ -13,6 +13,29 @@ namespace Assignment3
         {
             string input1 = Console.ReadLine();
             string input2 = Console.ReadLine();
+            int alphabetCountInInput1=0;
+            for(int i=0;i<input1.Length;i++)
+            {
+                if(input1[i]>=65&&input1[i]<=122)
+                {
+                    alphabetCountInInput1=1;
+                    break;
+                }
+            }
+            int alphabetCountInInput=0;
+            for(int i=0;i<input2.Length;i++)
+            {
+                if(input2[i]>=65&&input2[i]<=122)
+                {
+                    alphabetCountInInput2=1;
+                    break;
+                }
+            }
+            if(alphabetcount==1)
+            {
+                Console.WriteLine("string contains alphabets give correct input");
+        
+            }
             float m = float.Parse(input1);
             float n = float.Parse(input2);
             float finalFloat = FinalFloat.ConvertFloat(m, n);
