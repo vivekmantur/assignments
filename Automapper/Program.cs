34643e8694f60f897e44ff8b2fd147fa3d60d256
vivekmantur <vivek.mantur@cognine.com>
2024-09-01 16:51:38 +0530

    Update Program.cs

--- Automapper/Program.cs
+++ Automapper/Program.cs
@@ -8,7 +8,7 @@ namespace AutoMapperDemo
     {
         public static void Main()
         {
-            Employee<string, int, bool> emp = new Employee<string, int, bool>
+            Employee<string, int, bool> employeeone = new Employee<string, int, bool>
             {
                 Name = "James",
                 Salary = 2,
@@ -17,8 +17,8 @@ namespace AutoMapperDemo
                
             };
             var mapper = MapperConfig<string,int,bool>.InitializeAutomapper();
-            var employeeobject = mapper.Map<PermenantEmployee<string,int,bool>>(emp);
-            var permanentemployeeobject = mapper.Map<Employee<string,int,bool>, PermenantEmployee<string,int,bool>>(emp);
+            var employeeobject = mapper.Map<PermenantEmployee<string,int,bool>>(employeeone);
+            var permanentemployeeobject = mapper.Map<Employee<string,int,bool>, PermenantEmployee<string,int,bool>>(employeeone);
             Console.WriteLine("Name: " + permanentemployeeobject.Name + ", Salary: " + permanentemployeeobject.Sal + ", Address: " + permanentemployeeobject.Address + ", Department: " + permanentemployeeobject.Department+ "  Passport: "+permanentemployeeobject.Passport);
             Console.ReadLine();
         }
